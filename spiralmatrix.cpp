11ea3e4d58cb5dcc4af9b7df668ca5364f1d64b6
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2025-09-01 11:40:18 +0530

    SpiralMatrix Code

---
+++ spiralmatrix.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int total = row*col;
+        vector<int>ans;
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
+
+        while(count<total){
+            //for printing first row
+            for(int i=startingCol ; count<total && i<=endingCol;i++){
+                ans.push_back(matrix[startingRow][i]);
+                count++;
+            }
+            startingRow++;
+
+            //for printing last column
+            for(int i=startingRow ; count<total && i<=endingRow;i++){
+                ans.push_back(matrix[i][endingCol]);
+                count++;
+            }
+            endingCol--;
+
+            //for printing last row
+            for(int i=endingCol ; count<total && i>=startingCol;i--){
+                ans.push_back(matrix[endingRow][i]);
+                count++;
+            }
+            endingRow--;
+
+            for(int i=endingRow ; count<total && i>=startingRow;i--){
+                ans.push_back(matrix[i][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+};
