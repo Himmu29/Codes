5330643ed1b3e95218b01670bf838b39938d6403
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2025-09-02 12:52:16 +0530

    Majority Element2

---
+++ majorityElement2.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        vector<int> majorityElement(vector<int>& nums) {
+            int el1 = INT_MIN;
+            int el2 = INT_MIN;
+            int cnt1 = 0;
+            int cnt2 = 0;
+            for(int i=0;i<nums.size();i++){
+                if(cnt1 == 0 && nums[i]!=el2){
+                    cnt1 = 1;
+                    el1 = nums[i];
+                }
+                else if(cnt2 == 0 && nums[i]!=el1){
+                    cnt2 = 1;
+                    el2 = nums[i];
+                }
+                else if(el1 == nums[i]) cnt1++;
+                else if(el2 == nums[i]) cnt2++;
+                else{
+                    cnt1--;
+                    cnt2--;
+                }
+            }
+    
+            vector<int>ans;
+            int cnt3 = 0; int cnt4 = 0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i] == el1){
+                    cnt3++;
+                }
+                if(nums[i] == el2){
+                    cnt4++;
+                }
+            }
+    
+            int mini = nums.size()/3;
+            if(cnt3 > mini){
+                ans.push_back(el1);
+            }
+            if(cnt4 > mini){
+                ans.push_back(el2);
+            }
+            sort(ans.begin(),ans.end());
+            return ans;
+        }
+    };
